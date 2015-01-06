0f44f4c861f62073e1e99d4940a524b8f6a469d5
Nick Riebeek <nick.riebeek@gmail.com>
2015-01-05 20:40:03 -0500

    update events to only return those events for the current year

--- server/publications.js
+++ server/publications.js
@@ -2,5 +2,12 @@ Meteor.publish('allocations', function() {
 	return Allocations.find({userId: this.userId});
 });
 Meteor.publish('events', function() {
-	return Events.find({userId: this.userId});
+	var currentYear = new Date().getFullYear();
+	var lastDayOfPrevYear = currentYear - 1 + '-12-31';
+	var firstDayOfNextYear = currentYear + 1 + '-01-01';
+
+	return Events.find({
+		userId: this.userId, 
+		start: {$gt: lastDayOfPrevYear, $lt: firstDayOfNextYear}
+	});
 });
